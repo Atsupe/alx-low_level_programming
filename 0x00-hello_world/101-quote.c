30629af9cb17f8d0de1f06009602f00c0d781d02
Atsupe <carolamegatcher97@gmail.com>
2022-03-10 16:39:49 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - prints a quote
- *
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * follwed by a new line, to the standard error
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "\n and that piece of art is useful - Dora Korpar, 2012-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
