dc5760f356afbca84e22c81130ebf3dcd7c80abe
Atsupe <carolamegatcher97@gmail.com>
2022-03-11 17:05:39 -0800

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 
 /**
-* main - prints combination of numbers
-* Return: Always (Success)
-*/
+ * main - prints combination of numbers
+ * Return: Always (Success)
+ */
 int main(void)
 {
-int n, m l;
+	int 
 
-for (n = 48; n < 58; n++)
-{
-for (m = 49; m < 58; m++)
-{
-for (l = 50; l < 58; l++)
-{
-if (l > m && m > n)
-{
-putchar(n);
-putchar(m);
-putchar(l);
-if (n != 55 || m != 56)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+			if (l > m && m > n)
+			{
+   				putchar(n);
+				putchar(m);
+				putchar(l);
+				if (n != 55 || m != 56)
+				i{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
