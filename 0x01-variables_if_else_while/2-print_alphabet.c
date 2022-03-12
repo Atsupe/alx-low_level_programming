032d11421a6d0c76ac6347debd17e6b1bb0102bd
Atsupe <carolamegatcher97@gmail.com>
2022-03-11 16:12:18 -0800

    alpx

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main -prints all the alphabets in lowercase,
- * followed by a new line
- * Retun: Always 0 (Success)
+ * main - entry point
+ * 
+ * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-	
+		putchar(c);	
 	putchar('\n');
 	return (0);
 }
