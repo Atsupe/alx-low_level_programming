4ce56f703fc030f8cf47e50a3de2f8400008d0be
Atsupe <carolamegatcher97@gmail.com>
2022-03-15 17:11:42 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
  * followed by a new line
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char a;
 	int i;
@@ -24,4 +24,3 @@ void print_alphabet_x10(void);
 		i++;
 	}
 }
-
